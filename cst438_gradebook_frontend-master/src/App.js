be26a8286c69a66a03e248e66394e5bc26ca3024
Dale <78234740+Dale-S@users.noreply.github.com>
2023-09-25 23:26:17 -0700

    implement feature add student

--- cst438_gradebook_frontend-master/src/App.js
+++ cst438_gradebook_frontend-master/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import ListAssignment from './components/ListAssignment';
 import GradeAssignment from './components/GradeAssignment';
+import AddAssignment from './components/AddAssignment';
+import EditAssignment from './components/EditAssignment';
 
 function App() {
   return (
@@ -12,7 +14,10 @@ function App() {
             <Switch>
               <Route exact path="/" component={ListAssignment} />
               <Route path="/gradeAssignment" component={GradeAssignment} />
+              <Route path="/assignment/create" component={AddAssignment} />
+              <Route path="/assignment/edit" component={EditAssignment} />
               <Route render={ () => <h1>Page not found</h1>} />
+              
             </Switch>
           </div>
         </BrowserRouter>
--- cst438_gradebook_frontend-master/src/components/EditAssignment.js
+++ cst438_gradebook_frontend-master/src/components/EditAssignment.js
@@ -1,13 +1,148 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect}  from 'react';
+import {SERVER_URL} from '../constants';
+import {Link} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 
-function EditAssignment(props) { 
+//  required properties -  assignmentId
+//  
 
 
+function EditAssignment(props) {
+  
+  const headers = ['Assignment Name', 'Course Title', 'Due Date'];
+  let assignmentId=0;
+  const [assignment, setAssignment] = useState([]);
+  const [message, setMessage] = useState('');
+  const [date, setDate] = useState('');
+  const [name, setName] = useState('');
+  const [title, setTitle] = useState('');
+  const [inFormat, setFormat] = useState(true);
+
+  const path = window.location.pathname;  // /gradebook/123
+  const s = /\d+$/.exec(path)[0];
+  //console.log("Grade assignmentId="+s);
+  assignmentId=s;
+
+  const history = useHistory();
+
+  const backToHome = () => {
+    history.push('/');
+  };
+
+  const fetchAssignment = () => {
+    console.log("fetchAssignment");
+    fetch(`${SERVER_URL}/assignment/${assignmentId}`)
+    .then((response) => response.json() ) 
+    .then((data) => { 
+      console.log("assignment length "+data.length);
+      setAssignment(data);
+      setDate(data.dueDate);
+      setName(data.assignmentName);
+      setTitle(data.courseTitle);
+    }) 
+    .catch(err => console.error(err)); 
+  }
+
+  const onChangeDate = (e) => {
+    setMessage("");
+    if(/^\d{4}-\d{2}-\d{2}$/.test(e.target.value)){
+      console.log("in correct format")
+      setFormat(true);
+    }
+    else{
+      setMessage("Due Date must be in yyyy-mm-dd format");
+      setFormat(false);
+    }
+    setDate(e.target.value);
+  }
+  const onChangeName = (e) => {
+    console.log(name)
+    setName(e.target.value);
+  }
+
+  const saveEdit = () => {
+    setMessage(''); 
+    console.log("assignmentEdit.save");     
+    const as = {
+      "id" : assignment.id,
+      "assignmentName" : name,
+      "dueDate" : date,
+      "courseTitle" : title,
+      "courseId" : assignment.courseId
+    };
+    if(inFormat == true){
+      fetch(`${SERVER_URL}/assignment/update/${assignmentId}` , 
+        {  
+          method: 'PUT', 
+          headers: { 'Content-Type': 'application/json', }, 
+          body: JSON.stringify(as)
+        } )
+      .then(res => {
+        if (res.ok) {
+          fetchAssignment(assignmentId);
+          setMessage("Edit saved.");
+        } else {
+          setMessage("Save error. "+res.status);
+          console.error('Save edit error =' + res.status);
+      }})
+      .catch(err => {
+        setMessage("Exception. "+err);
+        console.error('Save edit exception =' + err);
+      });
+    }
+    else{
+      setMessage("Due Date must be in yyyy-mm-dd format, changes not saved");
+    }
+  }
+  
+  useEffect(() => {
+    fetchAssignment();
+  }, [] )
+  
   return (
-      <div>
-      </div>
-  ); 
+    <div>
+      <h3>Assignment Edit</h3>
+      <div margin="auto" >
+        <h4>{message}&nbsp;</h4>
+            <table className="Center"> 
+              <thead>
+                <tr>
+                  {headers.map((title, idx) => (<th key={idx}>{title}</th>))}
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <input
+                        name="assignmentName"
+                        value={name}  
+                        type="text"
+                        placeholder={name}
+                        onChange={(e) => onChangeName(e)}
+                    />
+                  </td>
+                  <td>{assignment.courseTitle}</td>
+                  <td>
+                    <input
+                        name="dueDate"
+                        value={date}  
+                        type="text"
+                        placeholder={date}
+                        onChange={(e) => onChangeDate(e)}
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <br></br>
+            <button id="sedit" type="button" margin="auto" onClick={saveEdit}>Save Changes</button>
+            <br></br>
+            <br></br>
+            <button id="home" type="button" margin="auto" onClick={backToHome}>Home Page</button>
+        </div>
+    </div>
+  );
 }
 
 export default EditAssignment;
