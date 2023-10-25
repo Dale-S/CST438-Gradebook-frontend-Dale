6ec4a0f0ecf0cbdab3f9835b53bbfb532896f39d
Dale-S <dalejsanchez2001@gmail.com>
2023-10-24 23:12:32 -0700

    changes 2

--- cst438_gradebook_frontend-master/src/App.js
+++ cst438_gradebook_frontend-master/src/App.js
@@ -4,22 +4,13 @@ import ListAssignment from './components/ListAssignment';
 import GradeAssignment from './components/GradeAssignment';
 import AddAssignment from './components/AddAssignment';
 import EditAssignment from './components/EditAssignment';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <h2>Gradebook</h2>
-      <BrowserRouter>
-          <div>
-            <Switch>
-              <Route exact path="/" component={ListAssignment} />
-              <Route path="/gradeAssignment" component={GradeAssignment} />
-              <Route path="/assignment/create" component={AddAssignment} />
-              <Route path="/assignment/edit" component={EditAssignment} />
-              <Route render={ () => <h1>Page not found</h1>} />
-            </Switch>
-          </div>
-        </BrowserRouter>
+      <Login />
     </div>
   );
 }
